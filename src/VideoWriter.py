9a34a1f1d3f11ccf5b5784856c02537cd516800d
Kopierkatze <alexandra.weiss@mailbox.tu-dresden.de>
2011-12-01 17:55:22 +0100

    first version of VideoWriter

--- src/VideoWriter.py
+++ src/VideoWriter.py
@@ -1,3 +1,5 @@
+import cv
+
 class VideoWriter(object):
   """creates a new video file where one can add frames to"""
   def __init__(self, file, width, height, fps, codec=None):
@@ -6,15 +8,21 @@ class VideoWriter(object):
 
     will raise error on file problems or invalid codecs.
     """
-    raise NotImplementedError()
+    # windows_feature: codec aus dialog waehlen bei codec = -1. wie verfahren???
+    writer = cv.CreateVideoWriter(file, 0, fps, cv.Size(width, height), 1) # uncompressed avi
 
   def addFrame(self, new_frame):
     """add the IplImage new_frame to the end of the video file.
 
     what happens on full disk???"""
-    raise NotImplementedError()
+    cr.WriteFrame(writer, new_frame)
 
   @classmethod
   def codecs(cls):
     """returns a dict that maps codec to human readable codec name"""
-    raise NotImplementedError()
+    # wie viele?? s. fourcc.org
+    codec = dict([('DIB' , 'uncompressed RGB'), ('FFDS' , 'ffdshow'), ('XVID' , 'XVID Mpeg4'), ('DIVX' , 'DivX')])
+    
+  def releaseWriter(self):
+  """closes VideoWriter after use """
+    cv.ReleaseVideoWriter(writer)
