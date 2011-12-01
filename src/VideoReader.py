839527950d258dcf487879ade6a77c4fe669d5c9
Kopierkatze <alexandra.weiss@mailbox.tu-dresden.de>
2011-12-01 17:55:10 +0100

    first version of VieoReader

--- src/VideoReader.py
+++ src/VideoReader.py
@@ -1,18 +1,30 @@
+import cv
+
 class VideoReader(object):
   """provides a image-by-image access to a video file"""
+  
   def __init__(self, filepath):
     """open the video file at filepath.
     will raise VideoOpenError on failure"""
-    raise NotImplementedError()
+    reader = cv.CaptureFromFile(filepath)
+    
 
   def frameAt(self, second):
     """returns the image that you would see when playing the video at second
     'second'"""
-    raise NotImplementedError()
+    
+    cv.SetCaptureProperty(reader, cv.CV_CAP_PROP_POS_MSEC, second*1000)
+    frame = cv.QueryFrame(reader) #IplImage 
+    return frame
 
   def duration(self):
     """duration of the video in seconds"""
-    raise NotImplementedError()
+    duration = (long(cv.GetCaptureProperty(video, cv.CV_CAP_PROP_POS_MSEC))) / 1000
+    return duration
 
   def fps(self):
-    raise NotImplementedError()
+    framerate = int(cv.GetCaptureProperty(reader, cv.CV_CAP_PROP_FPS))
+    return framerate    
+  def releaseReader(self):
+    """closes VideoReader after use """
+    cv.ReleaseCapture(reader)
