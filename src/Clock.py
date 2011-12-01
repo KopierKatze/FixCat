050457e51e1b0f87dfc937e98b5a10ff55ade266
Kopierkatze <alexandra.weiss@mailbox.tu-dresden.de>
2011-12-01 17:54:46 +0100

    first version of clock

--- src/Clock.py
+++ src/Clock.py
@@ -1,39 +1,67 @@
+import threading
+import time #for sleeping
+
 class Clock(object):
   """a clock!
      ticks in a specific interval. will call functions registered with
      'register()' on every tick. informs those funtions about current time
      in seconds.
      the time passed within one interval can be altered by a multiplicator."""
-  def __init__(self, interval = 1.0/30):
+  def __init__(self, maximal_duration, interval = 1.0/30):
     """creates clock with interval(time in the real world that passes between
     two ticks) and end_of_time(duration of video)"""
-    raise NotImplementedError()
+    
+    self.clock = 0.0
+    self.running = true
+    self.registered = []
+    self.multiplicator = 1
 
   def register(self, function):
     """register a funtion f to be called one every tick.
     f will get the current clock time as one float argument.
     e.g. f(2.3421)"""
-    raise NotImplementedError()
+    self.registered.append(function)
 
   def setMultiplicator(self, multi):
     """manipulates how much time of the clock passes within on interval.
        time of clock will be altered within one interval with value of
        interval * multi
     """
-    raise NotImplementedError()
+    self.multiplicator = multi
+    return self.multiplicator
 
-  def run(self):
-    """let the clock tick until end_of_time is reaced"""
-    raise NotImplementedError()
+  def run(self): 
+    """let the clock tick until end_of_time is reached"""    
+    # while(!self.stop && <max_duration>)
+    # sleep interval
+    # clock seek to +interval * multi 
+    while self.running and (clock < max_duration):
+        time.sleep(self.interval) # ??
+        seek(self.interval * self.multiplicator) # ??
+        self.interval *= self.interval
 
-  def stop(self):
+
+  def stop(self): 
     """stop the clock at current time"""
-    raise NotImplementedError()
+    self.running = false
 
   def seek(self, second):
     """set current time of clock to second
     if second > end_of_time a ClockError will be raised"""
-    raise NotImplementedError()
+    # immer zeit ueber seek aendern
+    # clock = second
+    # registrierte funktionen hier aufrufen (ueber liste aufrufen)
+    
+    self.clock = second
+    for function in registered:
+        function(self.clock)     
 
 class ClockError(Exception):
-  pass
+    pass
+  
+class ClockWorker(threading.Thread, Clock):
+    pass
+    
+    
+worker = ClockWorker()
+worker.start()
