a6d63bb60cfb69a0ebea052554b2172cc3e5f47e
Kopierkatze <alexandra.weiss@mailbox.tu-dresden.de>
2011-12-05 17:56:30 +0100

    first clock test method testing end of time

--- src/tests/Clock_test.py
+++ src/tests/Clock_test.py
@@ -3,9 +3,7 @@ import nose
 
 @nose.tools.raises(ClockError)
 def end_of_time_test(): #future todo: should use test generator
-    i = 5
+    i = 5 # if generator is present, maybe end_of_time_test gets parameters
     c = Clock(i)
-    #i = -5
     c.seek(i+0.1)
-    #c.seek(i)
     
