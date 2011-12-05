a6437882e7d5829605eef3c1b3ba7593f10344a9
Kopierkatze <alexandra.weiss@mailbox.tu-dresden.de>
2011-12-05 17:54:53 +0100

    first working test... only took me 3 days to figure out how, but hey...

--- src/tests/Clock_test.py
+++ src/tests/Clock_test.py
@@ -1,15 +1,11 @@
 from Clock import Clock, ClockError, ClockWorker
 import nose
 
-def test_generator():
-    
-    for i in range(0, 5):
-        yield end_of_time_test, i
-
-def end_of_time_test(i): #should use test generator
+@nose.tools.raises(ClockError)
+def end_of_time_test(): #future todo: should use test generator
+    i = 5
     c = Clock(i)
-    c.seek(i-0.1)
-    c.seek(i)
-    #nose.tools.raises(ClockError, c.seek, i+0.000001)
-
-
+    #i = -5
+    c.seek(i+0.1)
+    #c.seek(i)
+    
