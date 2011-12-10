80fc4debee6985188b37ae848b407859eb30aacd
Kopierkatze <alexandra.weiss@mailbox.tu-dresden.de>
2011-12-10 16:29:33 +0100

    first 2 tests for writer

--- src/tests/VideoWriter_test.py
+++ src/tests/VideoWriter_test.py
@@ -1,8 +1,15 @@
 import nose
 import os
 from VideoWriter import VideoWriter, WriterError
+import cv
 
-@nose.tools.raises(VideoWriter)
+@nose.tools.raises(WriterError)
 def init_test():
     writer = VideoWriter(None, 854, 428, 14, None)
+    
+
+@nose.tools.raises(WriterError)
+def addFrame_test():
+    writer = VideoWriter('/home/copycat/Desktop/pypsy/test_fuckyeah.avi', 854, 428, 14, cv.CV_FOURCC('X', 'V', 'I', 'D'))
+    writer.addFrame(None)
     
