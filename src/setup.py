a7662463db2e395527de0fbcbbd17ab699893113
Franz Gregor <rentafranz@gmail.com>
2012-02-04 23:43:14 +0100

    updated README, tweaked py2exe setup file

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,32 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['start_pypsy.py'],
-options={
-  'py2exe':{
-    'excludes':['email', 'Tkinter',],# 'unittest', 'setuptools', 'urllib', 'urllib2', 'compiler', 'distutils', 'doctest', 'logging', 'optparse', 'pdb', 'pydoc'],
-    'includes':['numpy'],
-    'optimize':2,
-    'bundle_files':3,
+setup(
+  console=['start_pypsy.py'],
+  zipfile=None, # integrate library into .exe
+  options={
+    'py2exe':{
+      'excludes':['_gtkagg', '_tkagg', 'bsddb',
+		  'curses', 'email', 'pywin.debugger',
+		  'pywin.debugger.dbgcon', 'pywin.dialogs',
+		  'tcl', 'Tkconstants', 'Tkinter', 'email',
+		  'pydoc_data', 'doctest', 'compiler',
+		  'xml', 'distutils', 'setuptools',
+                  'logging', 'pydoc', 'tarfile',
+                  'urllib', 'urllib2', 'cookielib',
+                  'optparse', 'xmlrpclib', 'zipfile',
+                  'httplib', 'xmllib', 'inspect',
+                  'urlparse', 'warnigns', 'ssl', '_ssl',
+                  'pdb', 'calendar',
+                  'pyreadline', 'pstats', 'ConfigParser',
+                  'plistlib', 'stringprep', 'gzip',],
+      'includes':['numpy'],
+      'optimize':2,
+      'bundle_files':3,
+      'ascii':False, # somehow we need encodings
+      'compressed':True,
+      'dll_excludes':['AVICAP32.dll', 'AVIFIL32.dll',
+                      'MSACM32.dll', 'MSVFW32.dll'],
     }
   }
 )
