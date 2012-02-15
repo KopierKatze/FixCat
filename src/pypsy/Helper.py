00f176b6e825566f9a6ff6df4cde21a8af28a8c3
Franz Gregor <rentafranz@gmail.com>
2012-02-15 13:37:36 +0100

    indentation fix for Helper.py

--- src/pypsy/Helper.py
+++ src/pypsy/Helper.py
@@ -3,11 +3,11 @@ quite generic and likely to be handy in other projects
 to."""
 
 def KeyCodeToHumanReadable(KeyCode):
-  """Return a human readable expression for
-  a wxPython key code."""
-  if not type(KeyCode) == int: return '?'
+    """Return a human readable expression for
+    a wxPython key code."""
+    if not type(KeyCode) == int: return '?'
 
-  if KeyCode in xrange(32, 256):
-    return chr(KeyCode)
+    if KeyCode in xrange(32, 256):
+        return chr(KeyCode)
 
-  return str(KeyCode)
+    return str(KeyCode)
